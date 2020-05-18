1f08f8d5bd6bd0c07196eda7c924957f186e9da1
Hissane <mouna.hissane@um5s.net.ma>
2020-05-18 23:07:20 +0000

    the whole project

---
+++ WSContact/src/data/DataContact.java
@@ -0,0 +1,18 @@
+package data;
+
+public class DataContact {
+	public static Contact [] lstContacts={
+			new Contact(0,"Bouslamti","Ahmed"),
+			new Contact(1,"Mouradi","Yasser"),
+			new Contact(2,"Elkamel","Karim"),
+			new Contact(3,"Mouradi","Ayman"),
+			new Contact(4,"Hissane","Mouna"),
+			new Contact(5,"Hissane","Samah"),
+			new Contact(6,"Ismaili","Ghita"),
+			new Contact(7,"Liaoui","Jihane"),
+			new Contact(8,"Ait-si","Aymane"),
+			new Contact(9,"Nasser","Samira"),
+			new Contact(10,"Mounadi","Imane"),
+			new Contact(11,"Nadir","Ali")
+	};
+}
